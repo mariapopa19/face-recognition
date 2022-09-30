2a19dd2873bb974f09f09233df9df6f3fca8756d
Maria Popa <maria_olimp@yahoo.com>
2022-09-30 22:27:36 +0300

    minor updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
+import Rank from './components/Rank/Rank';
 import ImageLinkForm from './ImageLinlForm/ImageLinkForm';
+import ParticlesBg from 'particles-bg';
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg color='#ffffff' num={30} type="cobweb"  bg={true} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,10 @@
+const Rank = () => {
+  return (
+    <>
+      <div className="white f3">{"Anderi, your current rank is... "}</div>
+      <div className="white f1">{"#5"}</div>
+    </>
+  );
+};
+
+export default Rank;
