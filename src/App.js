ab3eb4558311940e1b5dd6b6a8f1c667376d2780
Maria Popa <maria_olimp@yahoo.com>
2022-10-01 23:15:27 +0300

    face detection on am image

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import "./App.css";
 import Logo from "./components/Logo/Logo";
 import Navigation from "./components/Navigation/Navigation";
 import Rank from "./components/Rank/Rank";
-import ImageLinkForm from "./ImageLinlForm/ImageLinkForm";
+import ImageLinkForm from "./components/ImageLinlForm/ImageLinkForm";
 import ParticlesBg from "particles-bg";
 import Clarifai from "clarifai";
 import { useState } from "react";
-import FaceRecognition from "./components/FaceRecognition/FaceRecognitiom";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 
 function App() {
   const app = new Clarifai.App({
@@ -15,6 +15,24 @@ function App() {
 
   const [input, setInput] = useState("");
   const [imageURL, setImageURL] = useState("");
+  const [box, setBox] = useState({});
+
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  };
+
+  const displayFaceBox = (box) => {
+    setBox(box);
+  }
 
   const onInputChange = (event) => {
     console.log(event.target.value);
@@ -23,12 +41,10 @@ function App() {
 
   const onButtonSubmit = () => {
     setImageURL(input);
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, input).then(
-      function (response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function (err) {}
-    );
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, input)
+      .then((response) => displayFaceBox(calculateFaceLocation(response)))
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -41,7 +57,7 @@ function App() {
         onInputChange={onInputChange}
         onButtonSubmit={onButtonSubmit}
       />
-      <FaceRecognition imageURL={imageURL} />
+      <FaceRecognition box={box} imageURL={imageURL} />
     </div>
   );
 }
