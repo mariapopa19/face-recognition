899bbcf9dd37ff1ffe21b5aec56e19328264ce66
Maria Popa <maria_olimp@yahoo.com>
2022-10-01 17:25:50 +0300

    clarifai api add (old one)

--- src/App.js
+++ src/App.js
@@ -4,17 +4,31 @@ import Navigation from "./components/Navigation/Navigation";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./ImageLinlForm/ImageLinkForm";
 import ParticlesBg from "particles-bg";
+import Clarifai from "clarifai";
 import { useState } from "react";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognitiom";
 
 function App() {
+  const app = new Clarifai.App({
+    apiKey: "5fcf4b3a57d64bb8bf88155fb19d71e9",
+  });
+
   const [input, setInput] = useState("");
+  const [imageURL, setImageURL] = useState("");
 
   const onInputChange = (event) => {
     console.log(event.target.value);
+    setInput(event.target.value);
   };
 
   const onButtonSubmit = () => {
-    console.log("click");
+    setImageURL(input);
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, input).then(
+      function (response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function (err) {}
+    );
   };
 
   return (
@@ -23,11 +37,13 @@ function App() {
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onButtonSubmit} />
-      {/* <FaceRecognition /> */}
+      <ImageLinkForm
+        onInputChange={onInputChange}
+        onButtonSubmit={onButtonSubmit}
+      />
+      <FaceRecognition imageURL={imageURL} />
     </div>
   );
 }
 
 export default App;
-
---
+++ src/components/FaceRecognition/FaceRecognitiom.jsx
@@ -0,0 +1,11 @@
+const FaceRecognition = ({ imageURL }) => {
+  return (
+    <div className="center ma">
+        <div className='absolute mt2'>
+      <img alt="" src={imageURL} width='500px' height='auto' />
+      </div>
+    </div>
+  );
+};
+
+export default FaceRecognition;
