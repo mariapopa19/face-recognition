c31b26eca6d3a0906258a1f9caa9300a0ebbbd7c
Maria Popa <maria_olimp@yahoo.com>
2022-10-01 16:20:36 +0300

    console log

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import './App.css';
-import Logo from './components/Logo/Logo';
-import Navigation from './components/Navigation/Navigation';
-import Rank from './components/Rank/Rank';
-import ImageLinkForm from './ImageLinlForm/ImageLinkForm';
-import ParticlesBg from 'particles-bg';
+import "./App.css";
+import Logo from "./components/Logo/Logo";
+import Navigation from "./components/Navigation/Navigation";
+import Rank from "./components/Rank/Rank";
+import ImageLinkForm from "./ImageLinlForm/ImageLinkForm";
+import ParticlesBg from "particles-bg";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  const onButtonSubmit = () => {
+    console.log("click");
+  };
+
   return (
     <div className="App">
-      <ParticlesBg color='#ffffff' num={30} type="cobweb"  bg={true} />
+      <ParticlesBg color="#ffffff" num={30} type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onButtonSubmit} />
       {/* <FaceRecognition /> */}
     </div>
   );
 }
 
 export default App;
+
